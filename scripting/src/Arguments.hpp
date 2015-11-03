742803c78925fff2633e33e19ecbd11eed2df4dc
Phlar <r.prechtl@gmx.de>
2015-11-03 16:41:25 +0100

    Wip: Refactoring to support typed return values from LUA.

--- scripting/src/Arguments.hpp
+++ scripting/src/Arguments.hpp
@@ -1,18 +1,132 @@
 #pragma once
 
-#include "../../ai/src/IBlackboard.hpp"
-
 #include <boost/any.hpp>
 
+#include <memory>
+#include <sstream>
 #include <vector>
 
 namespace aw {
 namespace core {
 namespace scripting {
 
-// Variant of arguments that get passed to script resolvers as function parameters.
+// Container of 'any' arguments that get passed to script resolvers as function parameters.
 typedef boost::any Argument;
 typedef std::vector<Argument> ArgumentVector;
+
+// Return type also is of type 'any'.
+typedef boost::any ReturnValue;
+
+//! \brief A mere thin wrapper around a std::vector of results from a script's 
+//! execution as a "const vector" cannot be passed into the script-execution
+//! function without hitting the chance of getting modified in size.
+class ReturnValuesHolder {
+
+    public:
+
+        //@{
+        //! \brief Creator functions - I guess 4 return values are sufficient for now.
+        //! \todo: Refactor when using variadic templates.
+        static ReturnValuesHolder Create() {
+            return ReturnValuesHolder(0);
+        }
+
+        template<typename T1>
+        static ReturnValuesHolder Create() {
+            ReturnValuesHolder holder(1);
+            holder.m_returnValues[0] = std::unique_ptr<ReturnValue>(new ReturnValue(T1()));
+            return holder;
+        }
+
+        template<typename T1, typename T2>
+        static ReturnValuesHolder Create() {
+            ReturnValuesHolder holder(2);
+            holder.m_returnValues[0] = std::unique_ptr<ReturnValue>(new ReturnValue(T1()));
+            holder.m_returnValues[1] = std::unique_ptr<ReturnValue>(new ReturnValue(T2()));
+            return holder;
+        }
+
+        template<typename T1, typename T2, typename T3>
+        static ReturnValuesHolder Create() {
+            ReturnValuesHolder holder(3);
+            holder.m_returnValues[0] = std::unique_ptr<ReturnValue>(new ReturnValue(T1()));
+            holder.m_returnValues[1] = std::unique_ptr<ReturnValue>(new ReturnValue(T2()));
+            holder.m_returnValues[2] = std::unique_ptr<ReturnValue>(new ReturnValue(T3()));
+            return holder;
+        }
+
+        template<typename T1, typename T2, typename T3, typename T4>
+        static ReturnValuesHolder Create() {
+            ReturnValuesHolder holder(4);
+            holder.m_returnValues[0] = std::unique_ptr<ReturnValue>(new ReturnValue(T1()));
+            holder.m_returnValues[1] = std::unique_ptr<ReturnValue>(new ReturnValue(T2()));
+            holder.m_returnValues[2] = std::unique_ptr<ReturnValue>(new ReturnValue(T3()));
+            holder.m_returnValues[3] = std::unique_ptr<ReturnValue>(new ReturnValue(T4()));
+            return holder;
+        }
+        //@}
+
+        ReturnValuesHolder(ReturnValuesHolder&& holder) {
+
+            m_returnValues.swap(holder.m_returnValues);
+        }
+        
+        ReturnValuesHolder& operator= (ReturnValuesHolder&& holder) {
+
+            if(this != &holder) {
+                m_returnValues.swap(holder.m_returnValues);
+            }
+            return *this;
+        }
+
+        void SetValue(uint8_t index, const ReturnValue& value) {
+
+            verifyIndex(index);
+
+            // Do not assign another type than the one that is already stored.
+            if(m_returnValues[index]->type() != value.type()) {
+
+                std::stringstream errorMessage;
+                errorMessage << "Error assigning return value at index " << index << ".";
+                throw std::runtime_error(errorMessage.str());
+            }
+
+            m_returnValues[index] = std::unique_ptr<ReturnValue>(new ReturnValue(value));
+        }
+
+        const ReturnValue& GetValue(uint8_t index) {
+
+            verifyIndex(index);
+            return *m_returnValues[index];
+        }
+
+        uint8_t Size() const {
+
+            return static_cast<uint8_t>(m_returnValues.size());
+        }
+
+    protected:
+
+        ReturnValuesHolder(uint8_t size = 0)
+        : m_returnValues(size) {
+        }
+
+        void verifyIndex(uint8_t index) {
+
+            if(index >= m_returnValues.size()) {
+
+                std::stringstream errorMessage;
+                errorMessage << "Error accessing return value at index "
+                    << index << ", " << m_returnValues.size() 
+                    << " elements accessible.";
+
+                throw std::out_of_range(errorMessage.str());
+            }
+        }
+
+        std::vector<std::unique_ptr<ReturnValue>> m_returnValues;
+};
+
 
 } // namespace scripting
 } // namespace core
---
+++ scripting/testing/TestScriptingService_ReturnValuesHolder.cpp
@@ -0,0 +1,74 @@
+#define BOOST_TEST_DYN_LINK
+
+#define BOOST_TEST_MODULE "Test_ScriptingServiceReturnValuesHolder"
+
+#include <boost/test/unit_test.hpp>
+#include <boost/shared_ptr.hpp>
+
+#include "Arguments.hpp"
+
+namespace aw {
+namespace core {
+namespace scripting {
+namespace testing {
+
+namespace {
+
+class SomeClass {};
+typedef boost::shared_ptr<SomeClass> SomeClassPtr;
+
+const boost::any anyInt32 =       int32_t();
+const boost::any anyFloat =       float();
+const boost::any anyString =      std::string();
+const boost::any anyCustomClass = SomeClassPtr();
+
+} // anonymous namespace
+
+
+BOOST_AUTO_TEST_CASE(TestHolderNoContent) {
+
+    ReturnValuesHolder holder = ReturnValuesHolder::Create();
+    BOOST_CHECK_EQUAL(holder.Size(), 0);
+    BOOST_CHECK_THROW(holder.SetValue(0, anyInt32), std::out_of_range);
+    BOOST_CHECK_THROW(holder.GetValue(0), std::out_of_range);
+}
+
+BOOST_AUTO_TEST_CASE(TestHolderDifferentTypes) {
+
+    ReturnValuesHolder holder = ReturnValuesHolder::Create<int32_t, float, std::string>()   ;
+    BOOST_REQUIRE_EQUAL(holder.Size(), 3);
+
+    ReturnValue firstReturn, secondReturn, thirdReturn;
+    BOOST_REQUIRE_NO_THROW(firstReturn = holder.GetValue(0));
+    BOOST_REQUIRE_NO_THROW(secondReturn = holder.GetValue(1));
+    BOOST_REQUIRE_NO_THROW(thirdReturn = holder.GetValue(2));
+
+    // Check the types returned:
+    BOOST_CHECK(firstReturn.type() == anyInt32.type());
+    BOOST_CHECK(secondReturn.type() == anyFloat.type());
+    BOOST_CHECK(thirdReturn.type() == anyString.type());
+
+    // Negative test:
+    BOOST_CHECK(firstReturn.type() != anyCustomClass.type());
+    BOOST_CHECK(secondReturn.type() != anyCustomClass.type());
+    BOOST_CHECK(thirdReturn.type() != anyCustomClass.type());
+}
+
+BOOST_AUTO_TEST_CASE(TestHolderSettingData) {
+
+    ReturnValuesHolder holder = ReturnValuesHolder::Create<int32_t>();
+
+    // Store an int32
+    holder.SetValue(0, ReturnValue(int32_t(42)));
+    int32_t storedValue = 0;
+    BOOST_CHECK_NO_THROW(storedValue = boost::any_cast<int32_t>(holder.GetValue(0)));
+    BOOST_CHECK_EQUAL(storedValue, 42);
+
+    // Try storing another type.
+    BOOST_CHECK_THROW(holder.SetValue(0, ReturnValue(std::string("foo"))), std::runtime_error);
+}
+
+} // namespace testing
+} // namespace scripting
+} // namespace core
+} // namespace aw
